c5c2dfae1ff4cdaca2c297215051523fd8678799
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-09-12 14:28:01 +0200

    Prevent bleed of problem count between lint runs, fixes #4
    Introduce LinterError to differentiate between runtime and linter exceptions
    
    Remove test line
    
    Reintroduce detailed logging
    
    Fix problem counter
    
    Remove test line

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const CLIEngine = require("eslint").CLIEngine
-var counts = 0
 
 function createLinter (cli) {
   const fmt = cli.getFormatter()
   return (file) => {
     const report = cli.executeOnFiles([file])
     const msg = fmt(report.results)
-    if (msg === "") return
-    console.error(msg)
-    counts += parseInt(report.errorCount + report.warningCount)
+
+    if (msg.length > 0) {
+      console.warn(msg)
+    }
+
+    return {
+      errorCount: parseInt(report.errorCount),
+      warningCount: parseInt(report.warningCount)
+    }
   }
 }
+
+function sumProperty(key) {
+  return (count, obj) => count += obj[key]
+}
+
+function LinterError(message) {
+  this.name = 'LinterError'
+  this.message = message || ''
+}
+
+LinterError.prototype = Object.create(Error.prototype)
 
 module.exports = function () {
   this.eslint = function (opts) {
     const lint = createLinter(new CLIEngine(opts))
+
     return this.unwrap((files) => {
-      files.forEach(file => lint(file))
-      if (counts > 0) throw counts + " problems."
+      const problems = files.map(file => lint(file))
+        .filter((problem) => problem.errorCount > 0 || problem.warningCount > 0)
+
+      const errorCount = problems.reduce(sumProperty('errorCount'), 0)
+      const warningCount = problems.reduce(sumProperty('warningCount'), 0)
+
+      if (errorCount > 0 || warningCount > 0) {
+        throw new LinterError(errorCount + ' errors and ' + warningCount + ' warnings in ' + problems.length + ' files.')
+      }
     })
   }
 }
